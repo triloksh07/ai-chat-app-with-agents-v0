378f41155199041056f5aa75d386180dde7d650e
triloksh07 <trilok0726@gmail.com>
2025-10-16 23:53:52 +0530

    added: web analytics

--- react-stream-ai-assistant/src/main.tsx
+++ react-stream-ai-assistant/src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./providers/theme-provider.tsx";
+import { Analytics } from '@vercel/analytics/next';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <App />
+        <Analytics />
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
